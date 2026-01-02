ae8c2e2f01451aca84d2996532b10c27518e0efe
Manav S. Rajput <theexplainer007@gmail.com>
2026-01-02 09:22:53 +0530

    Add files via upload

---
+++ export csv.sql
@@ -0,0 +1,14 @@
+create table employee2(
+employee_id int primary key,
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+department varchar(20),
+salary numeric(10,2),
+joining_date date,
+age int
+
+);
+select *from employee2;
+
+-- Just click on table and add data dont forgot to enable the header option and csv stand for comma separted values and in csv format the delimeter is comma
+
